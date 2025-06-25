ca0177c99bb38948d4f1fefdcf6bf83791fe8b79
santo <santo@ALICELOMBARDI>
2025-06-25 20:59:25 +0100

    Adicionar arquivos de projeto.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("gestao-de-dados")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("gestao-de-dados")>
+<Assembly: AssemblyCopyright("Copyright © HP 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("62f54c13-4945-45e9-b45a-eaec1cd37bca")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
