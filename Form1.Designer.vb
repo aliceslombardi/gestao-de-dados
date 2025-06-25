a1adc2e5bfdf5b87b199d84c94b9db726b4ac27f
aliceslombardi <61299079+aliceslombardi@users.noreply.github.com>
2025-06-25 22:20:15 +0100

    Transformar Form1 em menu

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -20,12 +20,45 @@ Partial Class Form1
     'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
     'Pode ser modificado usando o Windows Form Designer.  
     'Não o modifique usando o editor de códigos.
+
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnAlunos = New System.Windows.Forms.Button()
+        Me.btnCursos = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnAlunos
+        '
+        Me.btnAlunos.Location = New System.Drawing.Point(12, 12)
+        Me.btnAlunos.Name = "btnAlunos"
+        Me.btnAlunos.Size = New System.Drawing.Size(120, 40)
+        Me.btnAlunos.TabIndex = 0
+        Me.btnAlunos.Text = "Gerir Alunos"
+        Me.btnAlunos.UseVisualStyleBackColor = True
+        '
+        'btnCursos
+        '
+        Me.btnCursos.Location = New System.Drawing.Point(12, 58)
+        Me.btnCursos.Name = "btnCursos"
+        Me.btnCursos.Size = New System.Drawing.Size(120, 40)
+        Me.btnCursos.TabIndex = 1
+        Me.btnCursos.Text = "Gerir Cursos"
+        Me.btnCursos.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(148, 110)
+        Me.Controls.Add(Me.btnCursos)
+        Me.Controls.Add(Me.btnAlunos)
+        Me.Name = "Form1"
+        Me.Text = "Menu"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents btnAlunos As System.Windows.Forms.Button
+    Friend WithEvents btnCursos As System.Windows.Forms.Button
 
 End Class
